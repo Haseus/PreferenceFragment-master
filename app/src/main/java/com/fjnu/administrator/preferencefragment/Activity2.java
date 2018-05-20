a376de6049d198c503f5af811fb3ea71aba3bcec
Haseus <835351928@qq.com>
2018-05-20 23:04:26 +0800

    Initial commit

---
+++ app/src/main/java/com/fjnu/administrator/preferencefragment/Activity2.java
@@ -0,0 +1,21 @@
+package com.fjnu.administrator.preferencefragment;
+
+import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+
+
+public class Activity2 extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        PreFra2 prefFragment = new PreFra2();
+        transaction.add(R.id.content,prefFragment);
+        transaction.commit();
+    }
+}
